2876203acc831a2505acee03d110dc70a5107ea3
Giovanni Molina <giovanni.m.05@gmail.com>
2025-01-28 13:54:57 -0300

    feat: add earthquake data fetching service using axios

--- src/services/earthquakeApi.js
+++ src/services/earthquakeApi.js
@@ -1,1 +1,19 @@
+import axios from 'axios'
 
+const BASE_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query'
+
+export async function fetchEarthquakes(startDate, minMagnitude) {
+  const params = {
+    format: 'geojson',
+    starttime: startDate,
+    minmagnitude: minMagnitude,
+  }
+
+  try {
+    const response = await axios.get(BASE_URL, { params })
+    return response.data.features
+  } catch (error) {
+    console.error('Error fetching earthquake data:', error)
+    return []
+  }
+}
