23431bdacc8a6151c0b97ce54264e8467122e645
Giovanni Molina <giovanni.m.05@gmail.com>
2025-01-29 10:43:49 -0300

    feat: add GitHub Actions workflow for deploying Vue 3 app to GitHub Pages and update Vite config for base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
@@ -12,4 +10,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: process.env.GITHUB_PAGES ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/` : '/',
 })
