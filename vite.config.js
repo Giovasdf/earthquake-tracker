bd787f4188c0bff7c2875c2ac8b71a31b3b3898e
Giovanni Molina <giovanni.m.05@gmail.com>
2025-01-30 11:17:51 -0300

    feat: configure Vite build settings with base path, output directory, and manifest generation

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/earthquake-tracker/', // 🔥 Asegura que está correctamente configurado
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: './index.html', // 🔥 Asegura que el build tome index.html correctamente
+      },
     },
   },
 })
